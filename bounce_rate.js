bf8c9c6bb4f0936a0ac1423ccb9a2b180858fa7b
sdeki <sdeki3388@gmail.com>
2016-09-21 23:02:29 +0800

    Update bounce_rate.js

--- bounce_rate.js
+++ bounce_rate.js
@@ -1,6 +1,7 @@
 function fixBounce(){
   var stored_height = $('#section1').outerHeight() + $('#section2').outerHeight() 
-  + $('#section3').outerHeight() + $('#section4').outerHeight() + $('#section5').outerHeight();
+  + $('#section3').outerHeight() + $('#section4').outerHeight() + $('#section5').outerHeight()
+  + $('#section6').outerHeight() + $('#section7').outerHeight();
   var a = 0;
   $(document).bind('scroll',function(){
     if (($(window).scrollTop() > stored_height) && (a == 0)) {
