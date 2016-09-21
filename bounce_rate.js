4e19cf717e193f33e35669732dd5f393fdd218e2
sdeki <sdeki3388@gmail.com>
2016-09-21 22:18:38 +0800

    Add files via upload

---
+++ bounce_rate.js
@@ -0,0 +1,12 @@
+function fixBounce(){
+  var stored_height = $('#section1').outerHeight() + $('#section2').outerHeight();
+  var a = 0;
+  $(document).bind('scroll',function(){
+    if (($(window).scrollTop() > stored_height) && (a == 0)) {
+     ga('send','event','fix-bounce-rate','scroll','over 50%');
+     a = 1;
+    }
+  })
+}
+
+$(document).ready(fixBounce);
